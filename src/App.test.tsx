65d535dd7bd927cedb77c748d3b8528c480127b1
5tarlight <developerstarlight@gmail.com>
2020-12-24 08:34:01 +0900

    delete test file

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
-
-test('renders Hello World text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
-});
