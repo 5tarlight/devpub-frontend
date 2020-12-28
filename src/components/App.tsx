ab61f50e86d5eb70e017cfec5b994982f7408285
Hyunjun Kim <hyunjun5216@naver.com>
2020-12-28 21:32:01 +0900

    login page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home, Register } from '../pages';
+import { Home, Register, Login } from '../pages';
 
 const App = () => {
   return (
     <Router>
       <Route exact path="/" component={Home} />
       <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
     </Router>
   );
 };
