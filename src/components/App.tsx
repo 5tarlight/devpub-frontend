e806c7447c2c412922c0f81210760c1b060102df
Hyunjun Kim <hyunjun5216@naver.com>
2020-12-28 21:10:59 +0900

    add Router (temp)

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home } from '../pages';
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Route path="/" component={Home} />
+    </Router>
+  );
+};
 
 export default App;
