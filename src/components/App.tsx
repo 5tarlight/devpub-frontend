3a973523922f69f5dea9a0ee8f6444f67004619f
Hyunjun Kim <hyunjun5216@naver.com>
2020-12-28 21:29:45 +0900

    register page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Home } from '../pages';
+import { Home, Register } from '../pages';
 
 const App = () => {
   return (
     <Router>
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route path="/register" component={Register} />
     </Router>
   );
 };
