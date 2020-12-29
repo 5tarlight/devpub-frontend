ca018e3e9f42a1685c1decb6de5c621b38f1f86c
5tarlight <developerstarlight@gmail.com>
2020-12-29 09:35:20 +0900

    change function component into arrow function

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NotFoundError } from '../components';
 
-function NotFound() {
+const NotFound = () => {
   return (
     <div>
       <NotFoundError />
     </div>
   );
-}
+};
 
 export default NotFound;
