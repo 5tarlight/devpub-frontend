7a71be17cb2c20ae1f47b2cddd7af70d58db129c
5tarlight <developerstarlight@gmail.com>
2020-12-30 14:18:29 +0900

    register inputs and password confirm message

--- src/components/auth/RegisterForm/RegisterForm.tsx
+++ src/components/auth/RegisterForm/RegisterForm.tsx
@@ -10,6 +10,7 @@ const RegisterForm: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirm, setConfirm] = useState('');
+  const [confirmMsg, setConfirmMsg] = useState('');
 
   const onEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
     const {
@@ -29,29 +30,40 @@ const RegisterForm: FC = () => {
     const {
       target: { value },
     } = e;
+
+    if (value !== password) {
+      setConfirmMsg('Password is not equal');
+    } else {
+      setConfirmMsg('');
+    }
+
     setConfirm(value);
   };
 
   return (
-    <div className={cx('login-form')}>
+    <div className={cx('register-form')}>
       <input
+        className={cx('register-input')}
         type={'email'}
         placeholder={'Email'}
         value={email}
         onChange={onEmailChange}
       />
       <input
+        className={cx('register-input')}
         type={'password'}
         placeholder={'Password'}
         value={password}
         onChange={onPasswordChange}
       />
       <input
+        className={cx('register-input')}
         type={'password'}
         placeholder={'Confirm Password'}
         value={confirm}
         onChange={onConfirmChange}
       />
+      <div className={cx('confirm-msg')}>{confirmMsg}</div>
       <input type={'submit'} value={'Sign Up'} />
     </div>
   );
