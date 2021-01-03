084effc56068cf18e0db7128dfbc31cde3ce013a
5tarlight <developerstarlight@gmail.com>
2021-01-03 23:32:27 +0900

    use router redirect smoooothly

--- src/components/Error/NotFound/NotFoundError.tsx
+++ src/components/Error/NotFound/NotFoundError.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import styles from './NotFoundError.scss';
 import classNames from 'classnames/bind';
+import { useHistory } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
-const moveHome = () => {
-  document.location.href = '/';
-};
 
 const NotFoundError = () => {
+  const history = useHistory();
+
+  const redirect = () => {
+    history.push('/');
+  };
+
   return (
     <div className={cx('not-found')}>
       <div className={cx('nf-title')}>
-        <img src="/img/fox.png" />
+        <img src="/img/fox.png" alt={'(╯°□°）╯︵ ┻━┻'} />
         <h1>Oops! 잘못 찾아오셨습니다.</h1>
         <h3>이동하려는 주소가 올바른지 확인해주세요.</h3>
       </div>
-      <button className={cx('home-btn', 'no-drag')} onClick={moveHome}>
+      <button className={cx('home-btn', 'no-drag')} onClick={redirect}>
         메인으로 돌아가기
       </button>
     </div>
