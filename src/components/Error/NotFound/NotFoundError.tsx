2c369729c2a06bee9fe72b37b2f0b61c32d3de3a
Hyunjun Kim <hyunjun5216@naver.com>
2021-01-03 23:21:39 +0900

    notfound page

--- src/components/Error/NotFound/NotFoundError.tsx
+++ src/components/Error/NotFound/NotFoundError.tsx
@@ -3,11 +3,21 @@ import styles from './NotFoundError.scss';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
+const moveHome = () => {
+  document.location.href = '/';
+};
 
 const NotFoundError = () => {
   return (
     <div className={cx('not-found')}>
-      <h1>NotFound Page</h1>
+      <div className={cx('nf-title')}>
+        <img src="/img/fox.png" />
+        <h1>Oops! 잘못 찾아오셨습니다.</h1>
+        <h3>이동하려는 주소가 올바른지 확인해주세요.</h3>
+      </div>
+      <button className={cx('home-btn', 'no-drag')} onClick={moveHome}>
+        메인으로 돌아가기
+      </button>
     </div>
   );
 };
